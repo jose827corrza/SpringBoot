fb72948ba4830d45a6320888baf512a5ea3e73c0
Jose Corredor <jose96corrza@gmail.com>
2022-01-05 22:42:13 -0500

    First commit

---
+++ src/main/java/com/josedev/fundspringboot/FundSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.josedev.fundspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/josedev/fundspringboot/FundSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josedev.fundspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
